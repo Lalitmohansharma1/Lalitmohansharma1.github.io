1d9e6eadc2a660d80b8712b798c12234e2f633cf
Lalitmohansharma1 <lalit_ms@ar.iitr.ac.in>
2022-02-25 16:09:55 +0530

    updates

--- Newproject/countdays/models.py
+++ Newproject/countdays/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+import datetime
+from datetime import date
 # Create your models here.
+class initialdate(models.Model):
+    day=models.CharField(max_length=2)
+    month=models.CharField(max_length=2)
+    year=models.CharField(max_length=4)
+    finalday=models.CharField(max_length=2)
+    finalmonth=models.CharField(max_length=2)
+    finalyear=models.CharField(max_length=4)
+
+    def __str__(self):
+        # return f"{ self.finalday}-{self.finalmonth}-{self.finalyear} to {self.day}-{self.month}-{self.year}"
+        self.d0=date(int(self.finalyear),int(self.finalmonth),int(self.finalday))
+        self.d1=date(int(self.year),int(self.month),int(self.day))
+        self.Ndays=str((self.d0-self.d1).days)
+        return self.Ndays
--- Newproject/countdays/views.py
+++ Newproject/countdays/views.py
@@ -1,7 +1,13 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import initialdate
+import datetime
+from datetime import date
 
 # Create your views here.
 def index(request):
-    return render(request,"countdays/index.html")
+    
+
+    # daa=(date(int(df.finalyear),int(df.finalmonth),int(df.finalday))-date(int(da.year),int(da.month),int(da.day))).days
+    return render(request,"countdays/index.html",{
+        "days":initialdate.objects.all()
+    })
